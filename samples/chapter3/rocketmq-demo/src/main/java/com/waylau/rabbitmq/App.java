33d2f4084a31087fdb35ee6bc2adaf3afcb420ee
waylau <waylau521@gmail.com>
2016-08-18 20:05:47 +0800

    update

---
+++ samples/chapter3/rocketmq-demo/src/main/java/com/waylau/rabbitmq/App.java
@@ -0,0 +1,28 @@
+package com.waylau.rabbitmq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author <a href="http://waylau.com">Way Lau</a> 
+ * @date 2016年8月18日
+ */
+public class App {
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+	/**
+	 * 
+	 */
+	public App() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		LOGGER.info("Welcome to waylau.com!");
+	}
+
+}
