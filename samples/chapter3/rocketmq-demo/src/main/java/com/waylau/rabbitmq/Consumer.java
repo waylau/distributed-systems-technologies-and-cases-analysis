2263262243d495ad6ed8338bf6375e014e7e74ea
waylau <waylau521@gmail.com>
2016-08-19 22:32:37 +0800

    update

--- samples/chapter3/rocketmq-demo/src/main/java/com/waylau/rabbitmq/Consumer.java
+++ samples/chapter3/rocketmq-demo/src/main/java/com/waylau/rabbitmq/Consumer.java
@@ -14,25 +14,38 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 消费者
+ * 消费者，接收和处理消息
  * 
  * @author <a href="http://waylau.com">waylau.com</a>
  * @date 2016年8月18日
  */
 public class Consumer {
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
 	private static final String GROUP_NAME = "waylau_com_consumer_gorup";
+	private static final String NAME_SERVER = "10.30.22.108:9876";
+	
 	public static void main(String[] args) throws InterruptedException, MQClientException {
+		
+		// consumerGroup 必须唯一
 		DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(GROUP_NAME);
 
+		// 设置 Consumer 第一次启动是从队列头部开始消费还是队列尾部开始消费
+		// 如果非第一次启动，那么按照上次消费的位置继续消费
 		consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
 
+		// 设置 name server 地址
+		consumer.setNamesrvAddr(NAME_SERVER);
+		
+		// 订阅 topic
 		consumer.subscribe("TopicTest", "*");
 
+		// 监听消息
 		consumer.registerMessageListener(new MessageListenerConcurrently() {
 
 			@Override
-			public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+			public ConsumeConcurrentlyStatus consumeMessage(
+					List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
 				LOGGER.info(Thread.currentThread().getName() + " Receive New Messages: " + msgs);
 				return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
 			}
--- samples/chapter3/rocketmq-demo/src/main/java/com/waylau/rabbitmq/Producer.java
+++ samples/chapter3/rocketmq-demo/src/main/java/com/waylau/rabbitmq/Producer.java
@@ -5,41 +5,46 @@ import org.slf4j.LoggerFactory;
 
 import com.alibaba.rocketmq.client.exception.MQClientException;
 import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
-import com.alibaba.rocketmq.client.producer.LocalTransactionExecuter;
-import com.alibaba.rocketmq.client.producer.LocalTransactionState;
 import com.alibaba.rocketmq.client.producer.SendResult;
 import com.alibaba.rocketmq.common.message.Message;
 
 /**
- * 生产者
+ * 生产者，用于发送消息
  * 
  * @author <a href="http://waylau.com">waylau.com</a>
  * @date 2016年8月18日
  */
 public class Producer {
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
 	private static final String GROUP_NAME = "waylau_com_producer_gorup";
+	private static final String NAME_SERVER = "10.30.22.108:9876";
+	
 	public static void main(String[] args) throws MQClientException, InterruptedException {
+		
+		// producerGroup 必须唯一
 		DefaultMQProducer producer = new DefaultMQProducer(GROUP_NAME);
 
+		// 设置 name server 地址
+		producer.setNamesrvAddr(NAME_SERVER);
+		
+		// 在发送消息前，必须调用 start 方法来启动Producer，只需调用一次即可
 		producer.start();
 
-		for (int i = 0; i < 1000; i++) {
+		// 模拟发送十条数据
+		for (int i = 0; i < 10; i++) {
 			try {
-				Message msg = new Message("TopicTest", // topic
-						"TagA", // tag
-						("Hello RocketMQ " + i).getBytes()// body
-				);
+				Message msg = new Message( 
+			             // Message Topic
+			             "TopicTest",
+			             // Message Tag,对消息进行再归类,方便Consumer指定过滤条件在MQ服务器过滤
+			             "TagA",
+			             // Message Body,任何二进制形式的数据,需要Producer与Consumer协商好一致的序列化和反序列化方式
+			             ("Welcome to waylau.com! " + i).getBytes());
+				
+				// 发送消息
 				SendResult sendResult = producer.send(msg);
-				/*
-				LocalTransactionExecuter tranExecuter = new LocalTransactionExecuter() {
-
-					@Override
-					public LocalTransactionState executeLocalTransactionBranch(Message msg, Object arg) {
-						return null;
-					}
-				};*/
-				// producer.sendMessageInTransaction(msg, tranExecuter, arg)
+ 
 				LOGGER.info(sendResult.toString());
 			} catch (Exception e) {
 				LOGGER.error(e.getMessage());
@@ -47,6 +52,7 @@ public class Producer {
 			}
 		}
 
+		// 在应用退出前，可以销毁Producer对象
 		producer.shutdown();
 	}
 }
